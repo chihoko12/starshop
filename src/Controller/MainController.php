de300cf69be5d17582f153e7182b0303501af556
accn <neudeck@amiconsult.de>
2024-09-13 11:36:38 +0200

    feat: add twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,27 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
-        return new Response('<strong>Startshop</strong>: your monopoly-busting option for Starshop parts!');
+        $startshopCount = 457;
+
+        $myShip = [
+            'name' => 'USS LeafyCruiser (NCC-0001)',
+            'class' => 'Garden',
+            'captain' => 'Jean-Luc Pickles',
+            'status' => 'under construction',
+        ];
+
+        return $this->render('main/homepage.html.twig', [
+            'numberOfStarShips' => $startshopCount,
+            'myShip' => $myShip,
+            ]);
     }
 }
